3fd20cf3012ce9ab28d0e94222d097d4eaa30a31
Nicholas Henderson <nchender@sph-190157-bio.ddns.sph.umich.edu>
2019-09-19 11:31:31 -0400

    updated function so that users can specify residual tolerance

--- R/daarem.R
+++ R/daarem.R
@@ -1,6 +1,6 @@
 daarem <- function(par, fixptfn, objfn, ..., control=list()) {
 
-  control.default <- list(maxiter=2000, order=5, tol=1.e-08, mon.tol=0.01, cycl.mon.tol=0.0, kappa=25, alpha=1.2)
+  control.default <- list(maxiter=2000, order=5, tol=1.e-08, mon.tol=0.01, cycl.mon.tol=0.0, kappa=25, alpha=1.2, resid.tol=0.95)
   namc <- names(control)
   if (!all(namc %in% names(control.default))) {
     stop("unknown names in control: ", namc[!(namc %in% names(control.default))])
@@ -13,6 +13,7 @@ daarem <- function(par, fixptfn, objfn, ..., control=list()) {
   cycl.mon.tol <- control$cycl.mon.tol
   a1 <- control$alpha
   kappa <- control$kappa
+  resid.tol <- control$resid.tol
 
   num.params <- length(par)
   nlag <- min(control$order, ceiling(num.params/2))
@@ -21,8 +22,8 @@ daarem <- function(par, fixptfn, objfn, ..., control=list()) {
       ans <- daarem_base_objfn(par, fixptfn, objfn, maxiter, tol, mon.tol, 
                                cycl.mon.tol, a1, kappa, num.params, nlag, ...) 
   } else {
-      ans <- daarem_base_noobjfn(par, fixptfn, maxiter, tol, mon.tol, 
-                                 cycl.mon.tol, a1, kappa, num.params, nlag, ...) 
+      ans <- daarem_base_noobjfn(par, fixptfn, maxiter, tol, resid.tol, 
+                                 a1, kappa, num.params, nlag, ...) 
   }
   if(!ans$convergence) {
      warning("Algorithm did not converge")
